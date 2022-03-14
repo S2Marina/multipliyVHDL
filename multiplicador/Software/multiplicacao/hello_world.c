84a409ea30f3239989e388712b79f4a275342940
S2Marina <marina_silva98@hotmail.com>
2022-03-14 10:36:15 -0300

    multiplicador

---
+++ multiplicador/Software/multiplicacao/hello_world.c
@@ -0,0 +1,34 @@
+/*
+ * #define GO_BASE 0x21000
+ * #define PRT_BASE 0x21010
+ * #define M_BASE 0x21040
+ * #define MULT_BASE 0x21030
+ * #define RES_BASE 0x21020
+ *
+ */
+
+#include <stdio.h>
+
+#define go (int *) 0x21000
+#define prt (int *) 0x21010
+#define m (int *) 0x21040
+#define mult (int *) 0x21030
+#define res (int *) 0x21020
+
+int main()
+{
+  printf("Hello from Nios II!\n");
+  *go = 0;
+  *prt = 0;
+  *m = 10;
+  *mult = 10;
+
+  *go = 1;
+  printf("go %i x %i\n", *m, *mult);
+  if(*prt == 1){
+	  printf("entrou no if - prt voltou\n");
+	  printf("Resultado da multiplicacao %i x %i = %i\n", *m, *mult, *res);
+  }
+
+  return 0;
+}
---
+++ multiplicador/unsigned_multiply.vhd
@@ -0,0 +1,31 @@
+-- Quartus II VHDL Template
+-- Unsigned Multiply
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity unsigned_multiply is
+	port 
+	(
+		clock : in std_logic;
+		go_mult : in std_logic;
+		prt_mult : out std_logic;
+		a	   : in std_logic_vector (5 downto 0);
+		b	   : in std_logic_vector (5 downto 0);
+		res_mult  : out std_logic_vector (11 downto 0)
+	);
+
+end entity;
+
+architecture rtl of unsigned_multiply is
+begin
+   process (clock) begin	
+		if (rising_edge(clock)) then
+			if(go_mult = '1') then
+				res_mult <= std_logic_vector(unsigned(a) * unsigned(b));
+				prt_mult <= '1';
+			end if;
+		end if;
+	end process;
+end rtl;
